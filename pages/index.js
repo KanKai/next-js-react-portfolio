c954a989fca24b9001bd6c67435ce29211fedc86
jeerawat_k <>
2019-02-27 17:49:34 +0700

    basic fetch api with axios

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,60 @@
-import React from 'react';
-import BaseLayout from '../components/layouts/BaseLayout';
-import SuperComponent from '../components/SuperComponent';
+import React from "react";
+import BaseLayout from "../components/layouts/BaseLayout";
+import SuperComponent from "../components/SuperComponent";
+import axios from "axios";
 
 class Index extends SuperComponent {
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      title: 'I am index page'
+  static async getInitialProps() {
+    let userData = {};
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      userData = response.data;
+    } catch (err) {
+      console.error(err);
     }
 
-    console.log('constructor')
+    return { initialData: [1, 2, 3, 4], userData };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "I am index page"
+    };
+
+    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log('componentDidMount')
+    console.log("componentDidMount");
   }
 
   componentDidUpdate() {
-    console.log('componentDidUpdate')
+    console.log("componentDidUpdate");
   }
 
   componentWillUnmount() {
-    console.log('componentWillUnmount')
+    console.log("componentWillUnmount");
   }
 
   updateTitle = () => {
-    this.setState({ title: 'I am Update index page' })
-  }
+    this.setState({ title: "I am Update index page" });
+  };
 
   render() {
     const { title } = this.state;
-
+    const { initialData, userData } = this.props;
     return (
       <BaseLayout>
         <h1>I am Index Page from Class Component</h1>
-        <h2> { title } </h2>
-        <button onClick={ this.updateTitle }> Update </button>
+        <h2> {title} </h2>
+        <h2> {userData.title} </h2>
+        <button onClick={this.updateTitle}> Update </button>
       </BaseLayout>
-    )
+    );
   }
 }
 
